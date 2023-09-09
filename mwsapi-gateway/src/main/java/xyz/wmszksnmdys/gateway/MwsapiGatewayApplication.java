d73fd43e1bcc044927e7ddda13bfa8439e11d0b9
forlornMWS <mwszksnmdys@gmail.com>
2023-09-09 16:34:17 +0800

    feat: gateway服务

---
+++ mwsapi-gateway/src/main/java/xyz/wmszksnmdys/gateway/MwsapiGatewayApplication.java
@@ -0,0 +1,13 @@
+package xyz.wmszksnmdys.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwsapiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MwsapiGatewayApplication.class, args);
+    }
+
+}
---
+++ mwsapi-gateway/src/test/java/xyz/wmszksnmdys/gateway/MwsapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.wmszksnmdys.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwsapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
